2a88bd4094408bd2fe0e39f6f0bdaa886aea2d5b
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-03 10:46:52 +0200

    Añadidos test de clase View

--- test/ViewTest.java
+++ test/ViewTest.java
@@ -1,2 +1,52 @@
+import cod.mvc.View;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ViewTest {
+
+    /**
+     * Prueba la visualización de la velocidad de un coche
+     */
+    @Test
+    public void muestraVelocidad() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        View.muestraVelocidad("1234-ABC", 100);
+        String expectedOutput = "The speed of car 1234-ABC is 100\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Prueba la visualización de la velocidad de un coche con velocidad negativa
+     */
+    @Test
+    public void muestraVelocidadNegativa() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        View.muestraVelocidad("1234-ABC", -100);
+        String expectedOutput = "The speed of car 1234-ABC is -100\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Prueba la visualización de la velocidad de un coche con matrícula vacía
+     */
+    @Test
+    public void muestraVelocidadMatriculaVacia() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        View.muestraVelocidad("", 100);
+        String expectedOutput = "The speed of car  is 100\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
 }
