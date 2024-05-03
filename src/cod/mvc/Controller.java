5f314e706367a9748c03c75d33781efc67daf273
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-03 10:06:34 +0200

    Creado un coche de prueba

--- src/cod/mvc/Controller.java
+++ src/cod/mvc/Controller.java
@@ -1,7 +1,10 @@
 package cod.mvc;
 
-public class Main {
+public class Controller {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Model.crearCoche("1234ABC", "Seat", 120);
+        View.muestraVelocidad("1234ABC", Model.getVelocidad("1234ABC"));
+        Model.cambiarVelocidad("1234ABC", 60);
+        View.muestraVelocidad("1234ABC", Model.getVelocidad("1234ABC"));
     }
 }
