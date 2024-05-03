74f979344b6378db2149675720de473adfd56b30
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-03 09:49:30 +0200

    Constructor para crear coches creado

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -6,4 +6,8 @@ public class Model {
 
     private ArrayList<Coche> parking = new ArrayList<>();
 
+    public void crearCoche(String matricula, String modelo, int velocidad) {
+        Coche coche = new Coche(matricula, modelo, velocidad);
+        parking.add(coche);
+    }
 }
