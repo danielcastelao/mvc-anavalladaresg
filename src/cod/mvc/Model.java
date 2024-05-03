46ad51ad7007cbab9cdd44001d37052444353854
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-03 09:50:42 +0200

    Metodo para buscar un coche creado+

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -10,4 +10,19 @@ public class Model {
         Coche coche = new Coche(matricula, modelo, velocidad);
         parking.add(coche);
     }
+
+    /**
+     * Busca un coche en el parking por su matrícula
+     * @param matricula Matrícula del coche a buscar
+     * @return Coche encontrado o null si no existe
+     */
+    public Coche getCoche(String matricula) {
+        for (Coche coche : parking) {
+            if (coche.getMatricula().equals(matricula)) {
+                return coche;
+            }
+        }
+        return null;
+    }
+
 }
