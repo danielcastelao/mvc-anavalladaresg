9f99e6ed9c481ded889df676787ba67605acf812
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-24 09:48:16 +0200

    Añadido parametro Model

--- src/cod/mvc/controller/Observer.java
+++ src/cod/mvc/controller/Observer.java
@@ -1,7 +1,8 @@
 package cod.mvc.controller;
 
 import cod.mvc.model.Coche;
+import cod.mvc.model.Model;
 
 public interface Observer {
-    void update(Coche coche);
+    void update(Coche coche, Model model);
 }
