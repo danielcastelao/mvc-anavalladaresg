3fdfeef232aaf8b7848cc386e0f4216d18e1af03
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-31 09:04:59 +0200

    Método que muestra un coche

--- src/cod/mvc/view/View.java
+++ src/cod/mvc/view/View.java
@@ -1,5 +1,7 @@
 package cod.mvc.view;
 
+import cod.mvc.model.Coche;
+
 public class View {
     /**
      * Muestra la velocidad de un coche
@@ -11,5 +13,20 @@ public class View {
     public static boolean muestraVelocidad(String matricula, Integer velocidad) {
         System.out.println("The speed of car " + matricula + " is " + velocidad);
         return true;
+    }
+
+    /**
+     * Muestra un coche
+     *
+     * @param coche Coche a mostrar
+     */
+    public void mostrarCoche(Coche coche) {
+        if (coche != null) {
+            System.out.println("Matrícula: " + coche.matricula);
+            System.out.println("Modelo: " + coche.modelo);
+            System.out.println("Velocidad: " + coche.velocidad);
+        } else {
+            System.out.println("No se encontró el coche.");
+        }
     }
 }
